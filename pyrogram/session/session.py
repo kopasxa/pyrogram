9c3e78946870d1a73144a7b094daa29773d57946
Pavel Korolenko <pavelcorolenko535@gmail.com>
2024-09-08 22:40:09 +0300

    Update session.py

--- pyrogram/session/session.py
+++ pyrogram/session/session.py
@@ -100,56 +100,54 @@ class Session:
         self.loop = asyncio.get_event_loop()
 
     async def start(self):
-        while True:
-            self.connection = Connection(
-                self.dc_id,
-                self.test_mode,
-                self.client.ipv6,
-                self.client.proxy,
-                self.is_media
-            )
+        # while True:
+        self.connection = Connection(
+            self.dc_id,
+            self.test_mode,
+            self.client.ipv6,
+            self.client.proxy,
+            self.is_media
+        )
 
-            try:
-                await self.connection.connect()
+        try:
+            await self.connection.connect()
 
-                self.recv_task = self.loop.create_task(self.recv_worker())
+            self.recv_task = self.loop.create_task(self.recv_worker())
 
-                await self.send(raw.functions.Ping(ping_id=0), timeout=self.START_TIMEOUT)
+            await self.send(raw.functions.Ping(ping_id=0), timeout=self.START_TIMEOUT)
 
-                if not self.is_cdn:
-                    await self.send(
-                        raw.functions.InvokeWithLayer(
-                            layer=layer,
-                            query=raw.functions.InitConnection(
-                                api_id=await self.client.storage.api_id(),
-                                app_version=self.client.app_version,
-                                device_model=self.client.device_model,
-                                system_version=self.client.system_version,
-                                system_lang_code=self.client.lang_code,
-                                lang_code=self.client.lang_code,
-                                lang_pack="",
-                                query=raw.functions.help.GetConfig(),
-                            )
-                        ),
-                        timeout=self.START_TIMEOUT
-                    )
+            if not self.is_cdn:
+                await self.send(
+                    raw.functions.InvokeWithLayer(
+                        layer=layer,
+                        query=raw.functions.InitConnection(
+                            api_id=await self.client.storage.api_id(),
+                            app_version=self.client.app_version,
+                            device_model=self.client.device_model,
+                            system_version=self.client.system_version,
+                            system_lang_code=self.client.lang_code,
+                            lang_code=self.client.lang_code,
+                            lang_pack="",
+                            query=raw.functions.help.GetConfig(),
+                        )
+                    ),
+                    timeout=self.START_TIMEOUT
+                )
 
-                self.ping_task = self.loop.create_task(self.ping_worker())
+            self.ping_task = self.loop.create_task(self.ping_worker())
 
-                log.info("Session initialized: Layer %s", layer)
-                log.info("Device: %s - %s", self.client.device_model, self.client.app_version)
-                log.info("System: %s (%s)", self.client.system_version, self.client.lang_code)
-            except AuthKeyDuplicated as e:
-                await self.stop()
-                raise e
-            except (OSError, RPCError):
-                await self.stop()
-            except Exception as e:
-                log.warning(f"Session closed: {e}")
-                await self.stop()
-                raise e
-            else:
-                break
+            log.info("Session initialized: Layer %s", layer)
+            log.info("Device: %s - %s", self.client.device_model, self.client.app_version)
+            log.info("System: %s (%s)", self.client.system_version, self.client.lang_code)
+        except AuthKeyDuplicated as e:
+            await self.stop()
+            raise e
+        except (OSError, RPCError):
+            await self.stop()
+        except Exception as e:
+            log.warning(f"Session closed: {e}")
+            await self.stop()
+            raise e
 
         self.is_started.set()
 
